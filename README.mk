592baeb7b96f40f6e2da5b08f5d6a42f8f416e9e
Jeff Buchbinder <jbuchbinder@ravemobilesafety.com>
2011-09-14 16:50:43 -0400

    Slight adjustments and documentation.

---
+++ README.mk
@@ -0,0 +1,11 @@
+android_device_samsung_transform
+================================
+
+An attempt to modernize the Froyo Samsung Transform port to work with
+Gingerbread and beyond.
+
+Originally forked from
+
+https://github.com/DroidFreak/android_device_samsung_transform
+
+
